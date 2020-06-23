cbc4ec36b55ce0f020abf127eb1971ab19ac1ede
igor_orlov <thoronion@gmail.com>
2020-06-23 21:23:44 +0300

    Main bug fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 var userYears = prompt('Сколько вам лет?', '');
 
-if (Number(userYears) % 10 === 1 && Number(userYears) !== 11){
+if (Number(userYears) % 10 === 1 && Number(userYears) % 100 !== 11){
   alert('Вам ' + userYears + ' год');
   }
-else if (Number(userYears) % 10 === 2 && Number(userYears) !== 12){
+else if (Number(userYears) % 10 === 2 && Number(userYears) % 100 !== 12){
   alert('Вам ' + userYears + ' года');
   }
-else if (Number(userYears) % 10 === 3 && Number(userYears) !== 13){
+else if (Number(userYears) % 10 === 3 && Number(userYears) % 100 !== 13){
   alert('Вам ' + userYears + ' года');
   }
-else if (Number(userYears) % 10 === 4 && Number(userYears) !== 14){
+else if (Number(userYears) % 10 === 4 && Number(userYears) % 100 !== 14){
   alert('Вам ' + userYears + ' года');
   }
 else {
