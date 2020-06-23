157ce1dbb1439de22077b354b07341950b5d2b82
igor_orlov <thoronion@gmail.com>
2020-06-23 22:31:59 +0300

    Code refactored

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 var userYears = prompt('Сколько вам лет?', '');
 
-if (Number(userYears) % 10 === 1 && Number(userYears) % 100 !== 11){
+var remainderTen = Number(userYears) % 10;
+var remainderHund = Number(userYears) % 100;
+
+if (remainderTen === 1 && remainderHund !== 11){
   alert('Вам ' + userYears + ' год');
   }
-else if (Number(userYears) % 10 === 2 && Number(userYears) % 100 !== 12){
+else if (remainderTen > 1 && remainderTen < 5 && remainderHund !== 12 && remainderHund !== 13 && remainderHund !== 14){
   alert('Вам ' + userYears + ' года');
   }
-else if (Number(userYears) % 10 === 3 && Number(userYears) % 100 !== 13){
-  alert('Вам ' + userYears + ' года');
-  }
-else if (Number(userYears) % 10 === 4 && Number(userYears) % 100 !== 14){
-  alert('Вам ' + userYears + ' года');
-  }
+
 else {
   alert('Вам ' + userYears + ' лет');
   }
